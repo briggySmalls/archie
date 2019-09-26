4e3b92cfdb658bf350df5748f89fd7af20fedb79
briggySmalls <briggySmalls90@gmail.com>
2019-09-26 22:40:38 +0100

    Fix tempremental test

--- internal/drawers/plantuml_test.go
+++ internal/drawers/plantuml_test.go
@@ -39,9 +39,16 @@ func TestDraw(t *testing.T) {
 	// Assert result
 	lines := strings.Split(output, "\n")
 	assert.Equal(t, lines[0], "@startuml")
-	assert.Equal(t, lines[2], "    [OneChild]")
-	assert.Equal(t, lines[1], "package \"One\" {")
-	assert.Equal(t, lines[3], "}")
+	// Find the line with the parent object definition
+	var parentLine uint
+	for i, line := range lines[1:] {
+		if line == "package \"One\" {" {
+			parentLine = uint(i + 1)
+		}
+	}
+	assert.Equal(t, lines[parentLine], "package \"One\" {")
+	assert.Equal(t, lines[parentLine+1], "    [OneChild]")
+	assert.Equal(t, lines[parentLine+2], "}")
 	assert.Assert(t, is.Contains(lines, "[Two]"))
 	assert.Assert(t, is.Contains(lines, "[OneChild] -- [Two]"))
 	assert.Equal(t, lines[len(lines)-2], "@enduml")
