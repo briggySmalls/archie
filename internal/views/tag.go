7c3e71fd23945b8539410822c51ecd75540c3c37
briggySmalls <briggySmalls90@gmail.com>
2019-11-26 07:22:24 +0000

    Ditch attempt to remove logical blocks
    For the time-being this approach will display the elements of interest.
    Future attempts could exclude the non-tagged blocks

--- internal/views/tag.go
+++ internal/views/tag.go
@@ -30,9 +30,6 @@ func NewTagView(model *mdl.Model, scope mdl.Element, tag string) mdl.Model {
 	// Create a model from the model's root elements
 	view, err := CreateSubmodel(model, taggedElements, relatedElements)
 
-	// Remove elements that have no tag (logical dividers)
-	model = removeUntagged(model)
-
 	// We shouldn't error (we've pulled elements out sensibly)
 	panicOnError(err)
 	return view
@@ -51,20 +48,6 @@ func findElements(model *mdl.Model, scope mdl.Element, tag string) []mdl.Element
 		elements = append(elements, findElements(model, child, tag)...)
 	}
 	return elements
-}
-
-// TODO: Should there be a 'RemoveElement' function provided by model?
-func removeUntagged(model *mdl.Model, el mdl.Element) *mdl.Model {
-	newParentsMap := make(map[Element]Element)
-	for _, child := range model.Children(el) {
-		if len(child.Tags()) == 0 {
-			// The child is untagged, so remove it
-			for _, grandchild := range model.Children(child) {
-				// Rewire composition to skip element
-				newParentsMap[grandchild] = el
-			}
-		}
-	}
 }
 
 func containsString(haystack []string, needle string) bool {
