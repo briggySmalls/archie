d5b14efb1f6c2544abb4c1fe4c1baa3c0a68c9e0
briggySmalls <briggySmalls90@gmail.com>
2019-11-28 23:59:56 +0000

    Fix tests

--- cli/server/server_test.go
+++ cli/server/server_test.go
@@ -1,12 +1,9 @@
 package server
 
 import (
-    "github.com/briggysmalls/archie/core"
-    "github.com/briggysmalls/archie/core/writers"
-    "github.com/gorilla/mux"
-    "gotest.tools/assert"
     "net/http"
     "net/http/httptest"
+    "strings"
     "testing"
 )
 
@@ -28,20 +25,17 @@ associations:
     destination: user
 `
 
-func TestHomeHandler(t *testing.T) {
-    // Create the server
-    s := newServer(t)
-
+func TestLandscapeHandler(t *testing.T) {
     // Create a request to pass to our handler. We don't have any query parameters for now, so we'll
     // pass 'nil' as the third parameter.
-    req, err := http.NewRequest("GET", "/", nil)
+    req, err := http.NewRequest("POST", "diagram/landscape", strings.NewReader(model))
     if err != nil {
         t.Fatal(err)
     }
 
     // We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
     rr := httptest.NewRecorder()
-    handler := http.HandlerFunc(s.homeHandler)
+    handler := http.HandlerFunc(landscapeHandler)
 
     // Our handlers satisfy http.Handler, so we can call their ServeHTTP method
     // directly and pass in our Request and ResponseRecorder.
@@ -55,40 +49,24 @@ func TestHomeHandler(t *testing.T) {
 }
 
 func TestContextHandler(t *testing.T) {
-    // Create the server
-    s := newServer(t)
-
     // Create a request to pass to our handler. We don't have any query parameters for now, so we'll
     // pass 'nil' as the third parameter.
-    req, err := http.NewRequest("GET", "/user", nil)
+    req, err := http.NewRequest("POST", "diagram/context?scope=sound%20system", strings.NewReader(model))
     if err != nil {
         t.Fatal(err)
     }
 
     // We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
     rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(contextHandler)
 
-    // Need to create a router that we can pass the request through
-    // so that the vars will be added to the context
-    router := mux.NewRouter()
-    router.HandleFunc("/{item}", s.contextHandler)
-    router.ServeHTTP(rr, req)
+    // Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+    // directly and pass in our Request and ResponseRecorder.
+    handler.ServeHTTP(rr, req)
 
     // Check the status code is what we expect.
     if status := rr.Code; status != http.StatusOK {
         t.Errorf("handler returned wrong status code: got %v want %v",
             status, http.StatusOK)
     }
-}
-
-func newServer(t *testing.T) *server {
-    // Create the api
-    a, err := core.New(writers.MermaidStrategy{}, model)
-    assert.NilError(t, err)
-
-    // Create the server
-    s, err := NewServer(a)
-    assert.NilError(t, err)
-
-    return s.(*server)
 }
