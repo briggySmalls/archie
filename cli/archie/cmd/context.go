7a83d05bfdf21e5eb715f113cfa346e7230937e6
briggySmalls <briggySmalls90@gmail.com>
2020-03-07 03:40:13 +0000

    Fix further docstring mistake

--- cli/archie/cmd/context.go
+++ cli/archie/cmd/context.go
@@ -33,7 +33,7 @@ Children of the scoping element.
 
 [2] Relevant associated elements
 
-Those that are associated to one of the child elements of the scope, where either:
+Those that are associated to one of the main elements of interest, where either:
 - The parent is an ancestor of scope
 - It is a root element`,
 	Args: cobra.ExactArgs(1),
