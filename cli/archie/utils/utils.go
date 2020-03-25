97163b9fb02be74c84c710a71498032c8cf366e8
briggySmalls <sam.briggs@entia.co>
2020-03-25 16:02:36 +0000

    Create opportunity for different writers
    This adds a 'writer' field to the yaml config

--- cli/archie/utils/utils.go
+++ cli/archie/utils/utils.go
@@ -4,9 +4,11 @@ import (
 	"github.com/briggysmalls/archie"
 	"github.com/briggysmalls/archie/writers"
 	"gopkg.in/yaml.v2"
+	"fmt"
 )
 
 type config struct {
+	Writer string `yaml:""`
 	Footer string `yaml:""`
 }
 
@@ -23,13 +25,22 @@ func ReadModel(modelAndConfig []byte) (archie.Archie, error) {
 	if err != nil {
 		return nil, err
 	}
-	// Create an Archie from the model & config
+	// Get the model as yaml
 	model, err := yaml.Marshal(p.Model)
 	if err != nil {
 		return nil, err
 	}
 	// Create a writer, using the provided config
-	writer := writers.PlantUmlStrategy{CustomFooter: p.Config.Footer}
+	var writer writers.Strategy
+	switch p.Config.Writer {
+	case "":
+		// Assume plantuml if not specified explicitly
+		fallthrough
+	case "plantuml":
+		writer = writers.PlantUmlStrategy{CustomFooter: p.Config.Footer}
+	default:
+		return nil, fmt.Errorf("Unexpected writer strategy: %s", p.Config.Writer)
+	}
 	// Create an archie instance with the writer and model
 	archie, err := archie.New(writer, string(model))
 	return archie, err
