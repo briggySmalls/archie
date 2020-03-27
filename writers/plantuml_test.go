8fc409af1e8d7322ec4fafafa77bf3b4d50cf1d0
briggySmalls <sam.briggs@entia.co>
2020-03-27 12:05:37 +0000

    Rewrite test to assert whole output

--- writers/plantuml_test.go
+++ writers/plantuml_test.go
@@ -3,8 +3,6 @@ package writers
 import (
 	"fmt"
 	"gotest.tools/assert"
-	is "gotest.tools/assert/cmp"
-	"strings"
 	"testing"
 )
 
@@ -22,26 +20,17 @@ skinparam ranksep 20
 	output, err := d.Write(*m)
 	assert.NilError(t, err)
 
+	const resultFormat = `@startuml
+package "One" <<software>> {
+	rectangle "OneChild" as %[1]s
+}
+rectangle "Two" as %[2]s <<software>><<mechanical>>
+%[1]s --> %[2]s
+skinparam shadowing false
+skinparam nodesep 10
+skinparam ranksep 20
+@enduml
+`
 	// Assert result
-	lines := strings.Split(output, "\n")
-	assert.Equal(t, lines[0], "@startuml")
-	// Find the line with the parent object definition
-	parentString := "package \"One\" <<software>> {"
-	assert.Assert(t, is.Contains(lines, parentString))
-	var parentLine uint
-	for i, line := range lines[1:] {
-		if line == parentString {
-			parentLine = uint(i + 1)
-		}
-	}
-	assert.Equal(t, lines[parentLine+1], fmt.Sprintf("    rectangle \"OneChild\" as %s", elMap["OneChild"].ID()))
-	assert.Equal(t, lines[parentLine+2], "}")
-	assert.Assert(t, is.Contains(lines, fmt.Sprintf("actor \"User\" as %s", elMap["User"].ID())))
-	assert.Assert(t, is.Contains(lines, fmt.Sprintf("rectangle \"Two\" as %s <<software>><<mechanical>>", elMap["Two"].ID())))
-	assert.Assert(t, is.Contains(lines, fmt.Sprintf("%s --> %s", elMap["OneChild"].ID(), elMap["Two"].ID())))
-
-	assert.Assert(t, is.Contains(lines, "skinparam shadowing false"))
-	assert.Assert(t, is.Contains(lines, "skinparam nodesep 10"))
-	assert.Assert(t, is.Contains(lines, "skinparam ranksep 20"))
-	assert.Equal(t, lines[len(lines)-2], "@enduml")
+	assert.Equal(t, output, fmt.Sprintf(resultFormat, elMap["OneChild"].ID(), elMap["Two"].ID()))
 }
