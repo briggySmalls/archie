16cb018d74aac10f7fac333a46c153bf4d1de865
briggySmalls <briggySmalls90@gmail.com>
2020-04-05 14:38:44 +0100

    WIP: Start building request

--- docs/src/components/diagram.js
+++ docs/src/components/diagram.js
@@ -2,12 +2,31 @@ import React from "react"
 import PropTypes from "prop-types"
 import axios from "axios"
 
-const Diagram = ({ type, scope, tag }) => {
+class Diagram extends React.Component {
+  async componentDidMount() {
+    // Build the request URL
+    let endpoint = new URL(`http://localhost:3000/diagrams/${this.type}`)
+    let params = {}
+    if (this.scope) {
+      params['scope'] = this.scope
+    }
+    if (this.tag) {
+      params['tag'] = this.tag
+    }
+    // Make the request
+    const response = await axios.post(endpoint, model)
+    // Save the response
+
+  }
+}
+
+const Diagram = ({ model, type, scope, tag }) => {
   // Request the
   return <div><p>Type: { type }</p><p>Scope: { scope }</p><p>Tag: { tag }</p></div>;
 }
 
 Diagram.propTypes = {
+  model: PropTypes.string.isRequired, // TODO: Find out why isRequired seems ineffectual
   type: PropTypes.string.isRequired, // TODO: Find out why isRequired seems ineffectual
   scope: PropTypes.string,
   tag: PropTypes.string,
