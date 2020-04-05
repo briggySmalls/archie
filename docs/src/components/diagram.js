17fbfcda6577ca2d3e93ddee19df2ae621653509
briggySmalls <briggySmalls90@gmail.com>
2020-04-05 13:13:21 +0100

    WIP: start thinking about making HTTP requests

--- docs/src/components/diagram.js
+++ docs/src/components/diagram.js
@@ -1,12 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
+import axios from "axios"
 
 const Diagram = ({ type, scope, tag }) => {
+  // Request the
   return <div><p>Type: { type }</p><p>Scope: { scope }</p><p>Tag: { tag }</p></div>;
 }
 
 Diagram.propTypes = {
-  type: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired, // TODO: Find out why isRequired seems ineffectual
   scope: PropTypes.string,
   tag: PropTypes.string,
 }
