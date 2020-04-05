ca6b43ea1ea20d32bf7103e35ff2bef3eb541d82
briggySmalls <briggySmalls90@gmail.com>
2020-04-05 14:39:02 +0100

    Add path prefix for github pages domain

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: `/archie`,
   siteMetadata: {
     title: `Archie`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
