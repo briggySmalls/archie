64a93a06f058636db2c32ff8e8e8cb2d0deb9686
briggySmalls <briggySmalls90@gmail.com>
2020-04-05 13:04:30 +0100

    WIP: Start adding props

--- docs/src/components/diagram.js
+++ docs/src/components/diagram.js
@@ -1,16 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-const Diagram = ({ siteTitle }) => {
-  return <div>{ siteTitle }</div>;
+const Diagram = ({ type, scope, tag }) => {
+  return <div><p>Type: { type }</p><p>Scope: { scope }</p><p>Tag: { tag }</p></div>;
 }
 
 Diagram.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Diagram.defaultProps = {
-  siteTitle: `default`,
+  type: PropTypes.string.isRequired,
+  scope: PropTypes.string,
+  tag: PropTypes.string,
 }
 
 export default Diagram
