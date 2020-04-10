e6ff7bd4e72691dbc4cc023d02e76841e5e8ad5f
briggySmalls <briggySmalls90@gmail.com>
2020-04-10 02:14:09 +0100

    Add material-ui libarary

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -3,6 +3,8 @@ import SEO from "../components/seo"
 import { Link } from "gatsby"
 import React from 'react'
 import { graphql } from 'gatsby'
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 const HomePage = ({data}) => {
   return (
@@ -10,18 +12,21 @@ const HomePage = ({data}) => {
       <SEO title="Home" />
       <h1>Systems engineering without the bloat</h1>
       <p>Archie is a lightweight tool for generating model-based architecture diagrams.</p>
-      <p>Define a model...</p>
-      <pre>{data.model.value}</pre>
-      <p>Generate diagrams..</p>
-      <div>
-        <h2>Context of A</h2>
-        <div dangerouslySetInnerHTML={{ __html: data.scopeA.value }} />
-      </div>
-      <div>
-        <h2>Context of B</h2>
-        <div dangerouslySetInnerHTML={{ __html: data.scopeB.value }} />
-      </div>
-      <Link to="/overview/">Overview</Link>
+      <Link to="/overview/"><Button color="primary">Learn more...</Button></Link>
+      <Grid container spacing={3}>
+        <Grid item>
+          <h2>Define a model...</h2>
+          <pre>{data.model.value}</pre>
+        </Grid>
+        <Grid item>
+          <h2>Context of A</h2>
+          <div dangerouslySetInnerHTML={{ __html: data.scopeA.value }} />
+        </Grid>
+        <Grid item>
+          <h2>Context of C</h2>
+          <div dangerouslySetInnerHTML={{ __html: data.scopeB.value }} />
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
