cddd585cedcf0a365ebdde422ead4571bc17bb4a
Samuel Vasko <samuel@entia.co>
2020-06-16 11:24:37 +0100

    Implement filtering by tags

--- internal/views/structure.go
+++ internal/views/structure.go
@@ -5,7 +5,7 @@ import (
 )
 
 // NewStructureView creates a view that shows the children of the scoped element
-func NewStructureView(model *mdl.Model, scope mdl.Element) mdl.Model {
+func NewStructureView(model *mdl.Model, scope mdl.Element, tag string) mdl.Model {
 	var primaries []mdl.Element
 	if scope == nil {
 		primaries = model.RootElements()
@@ -14,21 +14,39 @@ func NewStructureView(model *mdl.Model, scope mdl.Element) mdl.Model {
 	}
 	view := mdl.NewModel()
 	for _, primary := range primaries {
-		if primary.IsActor() {
+		if primary.IsActor() || hasOnlyOtherTags(tag, primary.Tags()) {
 			continue
 		}
 
 		view.AddRootElement(primary)
-		addChildren(model, &view, primary)
+		addChildren(model, &view, primary, tag)
 	}
 
 	return view
 }
 
-func addChildren(from, to *mdl.Model, of mdl.Element) {
+func addChildren(from, to *mdl.Model, of mdl.Element, tag string) {
 	for _, kid := range from.Children(of) {
+		if hasOnlyOtherTags(tag, kid.Tags()) {
+			continue
+		}
+
 		to.AddRootElement(kid)
 		to.AddAssociation(of, kid, []string{})
-		addChildren(from, to, kid)
+		addChildren(from, to, kid, tag)
 	}
+}
+
+func hasOnlyOtherTags(tag string, tags []string) bool {
+	if len(tags) == 0 || len(tag) == 0 {
+		return false
+	}
+
+	for _, t := range tags {
+		if t == tag {
+			return false
+		}
+	}
+
+	return true
 }
--- internal/views/structure_test.go
+++ internal/views/structure_test.go
@@ -8,16 +8,28 @@ import (
 	is "gotest.tools/assert/cmp"
 )
 
-// TestStrucureScoped Test generating non scoped diagram
-func TestStrucureWithutScope(t *testing.T) {
+// TestStrucureWithoutScope Test generating non scoped diagram
+func TestStrucureWithoutScope(t *testing.T) {
 	m := mdl.NewModel()
 	m.AddRootElement(mdl.NewItem("1", []string{}))
 	m.AddRootElement(mdl.NewItem("2", []string{}))
 	m.AddRootElement(mdl.NewActor("no"))
 
-	v := NewStructureView(&m, nil)
+	v := NewStructureView(&m, nil, "")
 	assert.Assert(t, is.Len(v.Elements, 2))
 	assert.Assert(t, is.Len(v.Associations, 0))
+}
+
+// TestStrucureWithTag Test generating scoped structure with tags
+func TestStrucureWithTag(t *testing.T) {
+	m := mdl.NewModel()
+	m.AddRootElement(mdl.NewItem("1", []string{"yes"}))
+	m.AddRootElement(mdl.NewItem("2", []string{"no"}))
+	m.AddRootElement(mdl.NewActor("no"))
+
+	v := NewStructureView(&m, nil, "yes")
+	assert.Assert(t, is.Len(v.Elements, 1))
+	assert.Equal(t, v.Elements[0].Name(), "1")
 }
 
 // TestStrucureScoped Test generating scoped structure diagram
@@ -29,7 +41,7 @@ func TestStrucureScoped(t *testing.T) {
 	m.AddRootElement(two)
 	m.AddElement(twokid, two)
 
-	v := NewStructureView(&m, two)
+	v := NewStructureView(&m, two, "")
 	assert.Assert(t, is.Len(v.Elements, 2))
 	assert.Assert(t, is.Len(v.Associations, 1))
 	assert.Assert(t, is.Equal(v.Associations[0].Source(), two))
