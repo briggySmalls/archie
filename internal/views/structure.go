16e8884658e50c89018c78f56d5a94bc8803e14e
Samuel Vasko <samvasko@gmail.com>
2020-06-23 23:27:42 +0100

    Prune branches without the specified tag

--- internal/views/structure.go
+++ internal/views/structure.go
@@ -14,43 +14,34 @@ func NewStructureView(model *mdl.Model, scope mdl.Element, tag string, maxDepth 
 	}
 	view := mdl.NewModel()
 	for _, primary := range primaries {
-		if primary.IsActor() || hasOnlyOtherTags(tag, primary.Tags()) {
+		if primary.IsActor() ||
+			(len(primary.Tags()) > 0 && len(tag) > 0 && !primary.HasTag(tag)) {
 			continue
 		}
 
-		view.AddRootElement(primary)
-		addChildren(model, &view, primary, tag, 1, maxDepth)
+		hasChildren := addChildren(model, &view, primary, tag, 1, maxDepth)
+		if hasChildren || len(tag) == 0 || primary.HasTag(tag) {
+			view.AddRootElement(primary)
+		}
 	}
 
 	return view
 }
 
-func addChildren(from, to *mdl.Model, of mdl.Element, tag string, depth int, maxDepth int) {
+func addChildren(from, to *mdl.Model, of mdl.Element, tag string, depth int, maxDepth int) bool {
 	if maxDepth != 0 && depth > maxDepth {
-		return
-	}
-
-	for _, kid := range from.Children(of) {
-		if hasOnlyOtherTags(tag, kid.Tags()) {
-			continue
-		}
-
-		to.AddRootElement(kid)
-		to.AddAssociation(of, kid, []string{})
-		addChildren(from, to, kid, tag, depth+1, maxDepth)
-	}
-}
-
-func hasOnlyOtherTags(tag string, tags []string) bool {
-	if len(tags) == 0 || len(tag) == 0 {
 		return false
 	}
+	atLeastOneChild := false
 
-	for _, t := range tags {
-		if t == tag {
-			return false
+	for _, kid := range from.Children(of) {
+		hasChildren := addChildren(from, to, kid, tag, depth+1, maxDepth)
+		if hasChildren || len(tag) == 0 || kid.HasTag(tag) {
+			atLeastOneChild = true
+			to.AddRootElement(kid)
+			to.AddAssociation(of, kid, []string{})
 		}
 	}
 
-	return true
+	return atLeastOneChild
 }
